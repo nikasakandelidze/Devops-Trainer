b7fba8fc7f70935c1b2b9754f66f1816a016f645
nika <nsaka16@freeuni.edu.ge>
2021-01-15 00:42:39 +0400

    updated terminal stuff

--- client/js/view/Animations/elementVerticalMovement/VerticalMovement.js
+++ client/js/view/Animations/elementVerticalMovement/VerticalMovement.js
@@ -1,11 +1,16 @@
 function animateLeftPaddingOfElementWithId(id, min, max, delta, displayType){
-    let element = document.querySelector(`#${id}`);
-    element.style.position='relative';
-    let leftPadding = min;
-    element.style.display=displayType;
-    let animate = setInterval(()=>{
-        if(leftPadding <= max) clearInterval(animate);
-        element.style.left=`${leftPadding}px`;
-        leftPadding+=delta;
-    }, 15);
+    return new Promise((resolve,reject) => {
+        let element = document.querySelector(`#${id}`);
+        element.style.position='relative';
+        let leftPadding = min;
+        element.style.display=displayType;
+        let animate = setInterval(()=>{
+            if(leftPadding <= max) {
+                clearInterval(animate);
+                resolve();
+            }
+            element.style.left=`${leftPadding}px`;
+            leftPadding+=delta;
+        }, 15);
+    });
 }
